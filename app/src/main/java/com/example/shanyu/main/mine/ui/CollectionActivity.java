097538bdaf4c75abc38b00b23953447bbde0c991
HEROCAT\天锁 <18868026919@163.com>
2021-05-15 17:01:38 +0800

    收藏列表新增左滑删除功能

--- app/src/main/java/com/example/shanyu/main/mine/ui/CollectionActivity.java
+++ app/src/main/java/com/example/shanyu/main/mine/ui/CollectionActivity.java
@@ -2,8 +2,7 @@ package com.example.shanyu.main.mine.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
+import android.widget.AbsListView;
 import android.widget.ListView;
 
 import com.example.shanyu.R;
@@ -15,25 +14,30 @@ import com.example.shanyu.main.home.ui.BookInfoActivity;
 import com.example.shanyu.main.mine.adapter.CollectionAdapter;
 import com.example.shanyu.main.mine.bean.CollectionBean;
 import com.example.shanyu.utils.SharedUtil;
+import com.example.shanyu.widget.MyCommonDialog;
 import com.example.shanyu.widget.MyRefreshLayout;
+import com.example.shanyu.widget.SwipeListLayout;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class CollectionActivity extends BaseActivity implements MyRefreshLayout.RefreshListener, AdapterView.OnItemClickListener {
+public class CollectionActivity extends BaseActivity implements MyRefreshLayout.RefreshListener, AbsListView.OnScrollListener, CollectionAdapter.OnClick {
 
     @BindView(R.id.myRefreshLayout)
     public MyRefreshLayout myRefreshLayout;
     @BindView(R.id.mListView)
     public ListView mListView;
-
     List<CollectionBean> collectionBeans;
+    private Set<SwipeListLayout> sets = new HashSet();
+    private CollectionAdapter mCollectionAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +50,7 @@ public class CollectionActivity extends BaseActivity implements MyRefreshLayout.
     @Override
     public void initView() {
         myRefreshLayout.setRefreshListener(this);
+        mListView.setOnScrollListener(CollectionActivity.this);
         showLoading();
         getCollection();
     }
@@ -70,8 +75,8 @@ public class CollectionActivity extends BaseActivity implements MyRefreshLayout.
                 collectionBeans = new Gson().fromJson(resultData, new TypeToken<List<CollectionBean>>() {
                 }.getType());
                 if (collectionBeans != null && collectionBeans.size() > 0) {
-                    mListView.setAdapter(new CollectionAdapter(CollectionActivity.this, collectionBeans));
-                    mListView.setOnItemClickListener(CollectionActivity.this);
+                    mCollectionAdapter = new CollectionAdapter(CollectionActivity.this, collectionBeans, sets, CollectionActivity.this);
+                    mListView.setAdapter(mCollectionAdapter);
                 }
             }
         });
@@ -84,10 +89,75 @@ public class CollectionActivity extends BaseActivity implements MyRefreshLayout.
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+        switch (scrollState) {
+            //当listview开始滑动时，若有item的状态为Open，则Close，然后移除
+            case SCROLL_STATE_TOUCH_SCROLL:
+                if (sets.size() > 0) {
+                    for (SwipeListLayout s : sets) {
+                        s.setStatus(SwipeListLayout.Status.Close, true);
+                        sets.remove(s);
+                    }
+                }
+                break;
+
+        }
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+    }
+
+    @Override
+    public void itemOnclick(int p) {
+
+        if (sets.size() > 0) {
+            for (SwipeListLayout s : sets) {
+                s.setStatus(SwipeListLayout.Status.Close, true);
+                sets.remove(s);
+            }
+        }
+
         Intent intent = new Intent(this, BookInfoActivity.class);
-        intent.putExtra("bookModeId", collectionBeans.get(position).getId().toString());
+        intent.putExtra("bookModeId", collectionBeans.get(p).getId().toString());
         startActivity(intent);
+    }
+
+    @Override
+    public void deletItemOnclick(int p) {
+
+        MyCommonDialog myCommonDialog = new MyCommonDialog(this);
+        myCommonDialog.setDialogContent("确认取消该收藏？");
+        myCommonDialog.setOnSetPositiveButton("取消", dialog -> {
+            dialog.dismiss();
+            cancleCollection(p);
+        });
+        myCommonDialog.setOnSetNegativeButton("返回", dialog -> dialog.dismiss());
+        myCommonDialog.show();
+
+    }
+
+    /**
+     * 取消收藏书籍
+     */
+    private void cancleCollection(int p) {
+        Map<String, String> map = new HashMap<>();
+        map.put("uid", SharedUtil.getIntence().getUid());
+        map.put("goods_id", collectionBeans.get(p).getId().toString());
+        HttpUtil.doGet(HttpApi.CANCLE_COLLECTION, map, new HttpResultInterface() {
+            @Override
+            public void onFailure(String errorMsg) {
+                dismissLoading();
+            }
+
+            @Override
+            public void onSuccess(String resultData) {
+                collectionBeans.remove(p);
+                mCollectionAdapter.notifyDataSetChanged();
+            }
+        });
+
     }
 
 }
